a7f5f3b6688c2cd001317bb1719eedfb3d437cbd
Asabon <asabin01@mail.ru>
2022-07-17 20:42:57 +0300

    6

---
+++ lesson_6_1.py
@@ -0,0 +1,10 @@
+# Пусть это будет модуль music_deserialize.py
+import json
+import pickle
+
+with open('group.json', 'r', encoding='utf-8') as f:
+    result = json.load(f)
+print(result)
+with open('group.pickle', 'rb') as f:
+    result = pickle.load(f)
+print(result)
